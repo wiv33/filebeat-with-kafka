b923172788987caf60d920a40bddbaa89fb75dd4
ps <wiv821253@gmail.com>
2021-01-24 00:11:37 +0900

    update docker file with gradle push

--- src/main/java/org/psawesome/filebeatforkafka/FilebeatForKafkaApplication.java
+++ src/main/java/org/psawesome/filebeatforkafka/FilebeatForKafkaApplication.java
@@ -1,18 +1,23 @@
 package org.psawesome.filebeatforkafka;
 
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
+import net.logstash.logback.encoder.org.apache.commons.lang3.builder.HashCodeExclude;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
 
 import java.time.Duration;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.logging.Logger;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 @Slf4j
 @SpringBootApplication
@@ -25,13 +30,38 @@ public class FilebeatForKafkaApplication {
   @Bean
   CommandLineRunner runner() {
     return args -> {
-      Flux.generate(sink -> sink.next(ThreadLocalRandom.current().nextInt()))
-              .delayElements(Duration.ofSeconds(1))
+      Flux.generate(sink -> sink.next(new StockInfo()))
+              .delayElements(Duration.ofMillis(333))
               .index()
-              .timestamp()
               .log()
               .subscribe();
       log.info("Hello ps world!!!");
     };
+  }
+
+}
+
+@ToString
+@EqualsAndHashCode
+@Getter(AccessLevel.PACKAGE)
+class StockInfo {
+
+  private final String title;
+  private final int price;
+  private final LocalDateTime dateTime;
+
+  public StockInfo() {
+    this.title = defaultTitle();
+    this.price = defaultPrice();
+    this.dateTime = LocalDateTime.now();
+  }
+
+  private String defaultTitle() {
+    var titleList = List.of("삼성전자", "현대모비스", "키움증권", "신세계", "삼성SDI");
+    return titleList.get(ThreadLocalRandom.current().nextInt(titleList.size() - 1));
+  }
+
+  private int defaultPrice() {
+    return ThreadLocalRandom.current().nextInt(-300_000, 300_000);
   }
 }
