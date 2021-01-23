d36bd62573d5b9f6ba1565c7c04c02dda8ec002b
ps <wiv821253@gmail.com>
2021-01-23 19:57:28 +0900

    generate log file by reactor

--- src/main/java/org/psawesome/filebeatforkafka/FilebeatForKafkaApplication.java
+++ src/main/java/org/psawesome/filebeatforkafka/FilebeatForKafkaApplication.java
@@ -5,6 +5,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.logging.Logger;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @Slf4j
 @SpringBootApplication
@@ -17,6 +25,12 @@ public class FilebeatForKafkaApplication {
   @Bean
   CommandLineRunner runner() {
     return args -> {
+      Flux.generate(sink -> sink.next(ThreadLocalRandom.current().nextInt()))
+              .delayElements(Duration.ofSeconds(1))
+              .index()
+              .timestamp()
+              .log()
+              .subscribe();
       log.info("Hello ps world!!!");
     };
   }
--- src/test/java/org/psawesome/elasticstackwithkafka/logback/CreateLoggingTests.java
+++ src/test/java/org/psawesome/elasticstackwithkafka/logback/CreateLoggingTests.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class CreateLoggingTests {
 
   Logger log = LoggerFactory.getLogger(this.getClass());
